f28be573d38f529fb402d4932a4c0e36ca7759fa
User22 <Vkozlov15012005@gmail.com>
2025-03-07 10:48:02 +0300

    Дизайн элемента на странице студента

--- StudentsVer2.0/View/Pages/Menu/StudentDetailPage.xaml.cs
+++ StudentsVer2.0/View/Pages/Menu/StudentDetailPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using StudentsVer2._0.Model;
+using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace StudentsVer2._0.View.Pages.Menu
 {
@@ -11,6 +14,9 @@ namespace StudentsVer2._0.View.Pages.Menu
         public StudentDetailPage(Student student, string groupTitle)
         {
             InitializeComponent();
+
+            DesignBorder.MouseLeftButtonDown += OnDesignElementClicked;
+
             SurnameTbl.Text = student.Surname;
             NameTbl.Text = student.Name;
             PatronymicTbl.Text = student.Patronymic ?? "Нет данных"; // Если отчество отсутствует
@@ -18,9 +24,15 @@ namespace StudentsVer2._0.View.Pages.Menu
             GroupTbl.Text = groupTitle;
         }
 
+        private void OnDesignElementClicked(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Элемент был кликнут!");
+        }
+
         private void BackBtn_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             this.NavigationService.Content = null;
         }
+
     }
 }
